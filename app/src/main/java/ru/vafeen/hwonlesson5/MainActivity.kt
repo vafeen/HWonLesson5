0062e8106e5a61593dc5613713acb97d0a8a418f
Vafeen <666av6@gmail.com>
2024-04-12 03:08:19 +0300

    Add putting and getting isRegistered in onSaveInstanceState, onCreate, onRestoreInstanceState

--- app/src/main/java/ru/vafeen/hwonlesson5/MainActivity.kt
+++ app/src/main/java/ru/vafeen/hwonlesson5/MainActivity.kt
@@ -1,17 +1,50 @@
 package ru.vafeen.hwonlesson5
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import ru.vafeen.hwonlesson5.databinding.ActivityMainBinding
+import ru.vafeen.hwonlesson5.values.PutGet
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var isRegistered = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        savedInstanceState.apply {
+            if (this != null) {
+                isRegistered = getBoolean(PutGet.isRegistered.value)
+            }
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.greeting.text = "Hello Android!"
+        binding.greeting.visibility = View.GONE
+    }
 
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.apply {
+            putBoolean(PutGet.isRegistered.value, isRegistered)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.apply {
+            isRegistered = getBoolean(PutGet.isRegistered.value)
+        }
     }
 }
